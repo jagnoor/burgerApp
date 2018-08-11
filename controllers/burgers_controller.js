14f4f325ca95c338574fa1bbbc7d21c16bdf44d8
jag <jagnoor@gmail.com>
2018-08-11 16:00:03 -0500

    commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,48 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+
+router.get("/", function(req, res) {
+  res.redirect("/burger");
+  });
+
+// get all the burgers, then redirec to /
+
+router.get("/burger", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+// make a new burger
+router.post("/burger/make", function(req, res) {
+  burger.insertOne(req.body.burger_name, function(result) {
+    res.redirect("/");
+  });
+});
+
+// change the burger to eaten
+router.put("/burger/eat/:id", function(req, res) {
+
+  burger.updateOne(req.params.id, function(result){
+    res.json("/");
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
 
 
 
-// ****************** Like controlla, controlla, yeah **********
+// ******************
 
 var express = require("express");
 
@@ -9,7 +50,7 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
+router.get("/burger", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
       burgers: data
